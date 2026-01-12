1cb2ab707cb387576dd488557fd28730f671dc9e
Greg Nagy <greg@dhamma.works>
2026-01-12 21:59:07 +0100

    Enable NU7 regtest activation at height 5
    Adds the necessary infrastructure to run NU7 on regtest with ./dev up --nu7:
    - Enables NU7 branch ID (0xffffffff) when built with zcash_unstable=nu7
    - Sets protocol version to 170_150 for NU7 builds
    - Configures regtest to activate NU7 at height 5 (Canopy=1, Nu5=2, Nu6=3, Nu6.1=4)
    - Documents LocalNetwork NU7 configuration in librustzcash

--- zebra-chain/src/parameters/network_upgrade.rs
+++ zebra-chain/src/parameters/network_upgrade.rs
@@ -236,8 +236,8 @@ pub(crate) const CONSENSUS_BRANCH_IDS: &[(NetworkUpgrade, ConsensusBranchId)] = 
     (Nu5, ConsensusBranchId(0xc2d6d0b4)),
     (Nu6, ConsensusBranchId(0xc8e71055)),
     (Nu6_1, ConsensusBranchId(0x4dec4df0)),
-    // TODO: set below to (Nu7, ConsensusBranchId(0x77190ad8)), once the same value is set in librustzcash
-    #[cfg(any(test, feature = "zebra-test"))]
+    // TODO: set to ConsensusBranchId(0x77190ad8) once finalized in ZIP
+    #[cfg(zcash_unstable = "nu7")]
     (Nu7, ConsensusBranchId(0xffffffff)),
     #[cfg(zcash_unstable = "zfuture")]
     (ZFuture, ConsensusBranchId(0xffffffff)),
